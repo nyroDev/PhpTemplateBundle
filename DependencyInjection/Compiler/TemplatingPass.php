69da1bc01b3b1ecbd6d11b63359882ecd3bf9c1d
nyro <cedric@nyrodev.com>
2022-01-07 15:08:42 +0100

    Fix tempaltingPass to correctly set assets and form helper

--- DependencyInjection/Compiler/TemplatingPass.php
+++ DependencyInjection/Compiler/TemplatingPass.php
@@ -24,10 +24,10 @@ class TemplatingPass implements CompilerPassInterface
 
     public function process(ContainerBuilder $container)
     {
-        if ($container->hasDefinition('nyrodev.templating.php.templating')) {
-            $refs = [];
-            $helpers = [];
+        $refs = [];
+        $helpers = [];
 
+        if ($container->hasDefinition('nyrodev.templating.php.templating')) {
             foreach ($container->findTaggedServiceIds(self::TEMPLATING_HELPER_TAG, true) as $id => $attributes) {
                 if (isset($attributes[0]['alias'])) {
                     $helpers[$attributes[0]['alias']] = $id;
@@ -39,27 +39,35 @@ class TemplatingPass implements CompilerPassInterface
                     throw new \Exception('Tag '.self::TEMPLATING_HELPER_TAG.' found, not alias provided');
                 }
             }
-
-            if (\count($helpers) > 0) {
-                $definition = $container->getDefinition('nyrodev.templating.php.templating');
-                $definition->addMethodCall('setHelpers', [$helpers]);
-
-                if ($container->hasDefinition('nyrodev.templating.engine.php.helpers_locator')) {
-                    $container->getDefinition('nyrodev.templating.engine.php.helpers_locator')->replaceArgument(0, $refs);
-                }
-            }
         }
 
         if ($container->hasDefinition('assets.packages')) {
+            $id = 'nyrodev.templating.helper.assets';
             $definitionAssets = new Definition(AssetsHelper::class);
             $definitionAssets->addTag(self::TEMPLATING_HELPER_TAG, ['alias' => 'assets']);
-            $container->setDefinition('nyrodev.templating.helper.assets', $definitionAssets);
+            $container->setDefinition($id, $definitionAssets);
+
+            $helpers['assets'] = $id;
+            $refs[$id] = new Reference($id);
         }
 
         if ($container->hasDefinition('twig.form.renderer')) {
+            $id = 'nyrodev.templating.helper.form';
             $definitionForm = new Definition(FormHelper::class);
             $definitionForm->addTag(self::TEMPLATING_HELPER_TAG, ['alias' => 'form']);
-            $container->setDefinition('nyrodev.templating.helper.form', $definitionForm);
+            $container->setDefinition($id, $definitionForm);
+
+            $helpers['form'] = $id;
+            $refs[$id] = new Reference($id);
+        }
+
+        if (\count($helpers) > 0) {
+            $definition = $container->getDefinition('nyrodev.templating.php.templating');
+            $definition->addMethodCall('setHelpers', [$helpers]);
+
+            if ($container->hasDefinition('nyrodev.templating.engine.php.helpers_locator')) {
+                $container->getDefinition('nyrodev.templating.engine.php.helpers_locator')->replaceArgument(0, $refs);
+            }
         }
     }
 }
